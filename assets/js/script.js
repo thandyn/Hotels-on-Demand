2da4b1d2121b82ae13313a9fbd8920192ceae131
siahmoymajid <122416542+siahmoymajid@users.noreply.github.com>
2023-03-10 13:42:53 -0800

    initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,21 @@
 
-document.getElementById('submit').addEventListener('click', function(e){
+document.getElementById('submitBtn').addEventListener('click', function(){
   var cityInput = document.getElementById('search-value').value;
-  console.log(cityInput);
-  if (!cityInput){
-    return;
+  console.log('city: ',cityInput);
+  if (!cityInput) return;
 
  fetch(
- https://proxy.cors.sh/https://api.yelp.com/v3/businesses/search?location=${cityInput}&term=hotels&sort_by=best_match&limit=20,
-      {
-      headers: {
-        Authorization:"Bearer KK1z_UNPqGdV5DE38Usm9M4nwWOtrX8NvxCEbWL2qAFNbMSGtpP3ce0_ZA6gw0vANgRlwIQYky9wCA5feMpcqGQCHoXchSkahkHXVudiEJdP0l4Pin-ntq0985YHZHYx", },
-        })
-        .then((response) => response.json())
-        .then((response) => console.log(response))
-        console.log(response)
-        .catch((err) => console.error(err));
-  }
-})
+   //  'https://proxy.cors.sh/https://api.yelp.com/v3/businesses/search?location=${cityInput}&term=hotels&sort_by=best_match&limit=20',
+   "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${cityInput}&term=hotels&sort_by=best_match&limit=1",
+   {
+     headers: {
+       Authorization:
+         "Bearer KK1z_UNPqGdV5DE38Usm9M4nwWOtrX8NvxCEbWL2qAFNbMSGtpP3ce0_ZA6gw0vANgRlwIQYky9wCA5feMpcqGQCHoXchSkahkHXVudiEJdP0l4Pin-ntq0985YHZHYx",
+     },
+   }
+ )
+   .then((response) => response.json())
+   .then((response) => console.log(response))
+   .catch((err) => console.error(err));
+});
+
